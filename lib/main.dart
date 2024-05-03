703c65ade2ef7be344cbb5ecb7d27552fd2ae015
Elias Walton <elias_walton@hotmail.com>
2024-05-03 17:59:05 -0600

    Este es el cuarto commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,10 @@ class ColorChangerWidget extends StatefulWidget{
 class _ColorChangerWidgetState extends State<ColorChangerWidget>{
   Color _currentColor = Colors.blue;
   
+  //Este es el metodo para cambiar el color al ser presionado
   void _changeColor(){
     setState(() {
+      //Aqui se cambia el color actual de azul a rojo y viceversa
       _currentColor = (_currentColor == Colors.blue) ? Colors.red : Colors.blue;
     });
   }
@@ -38,9 +40,11 @@ class _ColorChangerWidgetState extends State<ColorChangerWidget>{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Color Changing Widget")
+        title: Text("El Widget que Cambia de Color") //Titulo de la aplicacion
       ),
-      body: Center(
+      //Aqui se crea un cuerpo (el boton en este caso), se aplica el tamaño, fuente, color de la letra, etc, y se llaman
+      //los metodos que lo hacen funcionar
+      body: Center( 
         child: GestureDetector(
           onTap: _changeColor,
           child: Container(
